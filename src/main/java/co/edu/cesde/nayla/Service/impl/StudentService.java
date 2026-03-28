cfa7968b8b6b8646bf164e225f795ee337e0adca
karinatati71-cmd <karinatati71@gmail.com>
2026-03-28 13:39:18 -0500

    Refactor StudentService interface with methods

--- src/main/java/co/edu/cesde/nayla/Service/impl/StudentService.java
+++ src/main/java/co/edu/cesde/nayla/Service/impl/StudentService.java
@@ -1,2 +1,12 @@
-package co.edu.cesde.nayla.Service.impl;public interface StudentService {
+package co.edu.cesde.nayla.Service.impl;
+
+import co.edu.cesde.nayla.models.Student;
+
+public interface StudentService<list> {
+    Student create (Student student);
+    boolean delete (long studentId);
+    boolean update(Student studentsUpdate);
+    Student finById(Long studentId);
+    list<Student> findAll();
+
 }
